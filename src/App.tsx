d7cd68c7c5b4d1d60de859fe9a6f6f30d2e37007
Boonsom W <bworrawa@student.42.fr>
2026-09-24 15:43:17 +0700

    v4 is out

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
-          <h3 >Finally, VERSION III</h3>
+          <h3 >Oh no, V4 is out!</h3>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
